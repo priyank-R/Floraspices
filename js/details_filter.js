e389df17e982c5107718421fb82348a1480febe7
priyank-R <42401655+priyank-R@users.noreply.github.com>
2020-01-13 17:25:45 +0530

    made content modifs

---
+++ js/details_filter.js
@@ -0,0 +1,11 @@
+function info_uncollapse() {
+
+    document.getElementById('info').style.display = "block";
+    document.getElementById('import').style.display = "none";
+}
+
+function import_uncollapse() {
+
+    document.getElementById('info').style.display = "none";
+    document.getElementById('import').style.display = "block";
+}
